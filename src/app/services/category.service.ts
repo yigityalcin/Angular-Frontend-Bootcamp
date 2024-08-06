6a519175f114ea63e77198343887aa077b63de85
Yigit Yalcin <yigityalciin@hotmail.com>
2024-08-06 17:49:11 +0300

    Angular Part 3 Commit 1

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -2,17 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
-import { Product } from '../models/product';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class CategoryService {
   
-  apiUrl = "https://localhost:44348/api/products/getall"
+  apiUrl = "https://localhost:44348/api/categories/getall"
   constructor(private httpClient:HttpClient) { }
 
-  getProducts():Observable<ListResponseModel<Product>>{
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  getCategories():Observable<ListResponseModel<Category>>{
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
   }
 }
